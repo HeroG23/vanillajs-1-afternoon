c88f42c85228dac9bd04832394df974e9294fad4
Brian Demro <demrobrian@hotmail.com>
2020-10-13 18:04:47 -0500

    Blackdiamond part included

--- Part2/index.js
+++ Part2/index.js
@@ -10,3 +10,14 @@ function setCard() {
     card.style.color = colorInput.value;
   }
   
+const reset = ()=>{
+  const diamonds = document.getElementById('diamond')
+  const spades = document.getElementById('spades')
+  const clubs = document.getElementById('clubs')
+  const hearts = document.getElementById('hearts')
+
+  diamonds.style.color = 'grey'
+  spades.style.color = 'grey'
+  clubs.style.color = 'grey'
+  hearts.style.color = 'grey'
+}
